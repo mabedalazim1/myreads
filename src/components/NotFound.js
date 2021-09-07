92708510a1af743178fa92d85bb49e39c4682a17
mabedalazim1 <mabedalazim1@gmail.com>
2021-09-07 19:44:18 +0200

    Add components

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div> Not Found </div>
+        <div className="not-found">
+            This Page Is Not Found
+             <p>
+                <Link to="/">Go home</Link>
+            </p>
+        </div>
     )
 }
 
